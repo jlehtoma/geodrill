410de7bb8233c88dae2ed13fcb99330d032ecf25
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2016-01-18 18:46:31 +0200

    Add version numbering

---
+++ geodrill/GeoDrillConfig.h
@@ -0,0 +1,9 @@
+#ifndef GEODRILLCONFIG_H
+#define GEODRILLCONFIG_H
+
+// the configured options and settings for Tutorial
+#define GeoDrill_VERSION_MAJOR 0
+#define GeoDrill_VERSION_MINOR 0
+#define GeoDrill_VERSION_PATCH 1
+
+#endif
