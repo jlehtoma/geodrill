f7bcacd3596499f5b4bb5be2b78d8c820c004bf4
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2016-01-28 17:36:23 +0200

    Implement file_utils::list_files()

--- geodrill/file_utils.cpp
+++ geodrill/file_utils.cpp
@@ -1,4 +1,69 @@
-//
-// Created by jlehtoma on 1/28/16.
-//
+//  simple_ls program  -------------------------------------------------------//
 
+//  Copyright Jeff Garland and Beman Dawes, 2002
+
+//  Use, modification, and distribution is subject to the Boost Software
+//  License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+
+//  See http://www.boost.org/libs/filesystem for documentation.
+
+#define BOOST_FILESYSTEM_VERSION 3
+
+//  As an example program, we don't want to use any deprecated features
+#ifndef BOOST_FILESYSTEM_NO_DEPRECATED
+#  define BOOST_FILESYSTEM_NO_DEPRECATED
+#endif
+#ifndef BOOST_SYSTEM_NO_DEPRECATED
+#  define BOOST_SYSTEM_NO_DEPRECATED
+#endif
+
+#include "boost/filesystem/operations.hpp"
+#include "boost/filesystem/path.hpp"
+#include "boost/progress.hpp"
+#include <iostream>
+
+namespace fs = boost::filesystem;
+
+void list_files(std::string dir, std::vector<std::string>& raster_files)
+{
+    fs::path p(fs::current_path());
+
+    p = fs::system_complete(dir.c_str());
+
+    unsigned long file_count = 0;
+    unsigned long err_count = 0;
+
+    if (!fs::exists(p))
+    {
+        std::cout << "\nNot found: " << p << std::endl;
+    }
+
+    if (fs::is_directory(p))
+    {
+        p = fs::canonical(p);
+        std::cout << "\nIn directory: " << p << "\n\n";
+        fs::directory_iterator end_iter;
+        for (fs::directory_iterator dir_itr(p); dir_itr != end_iter; ++dir_itr)
+        {
+            try
+            {
+                if (fs::is_regular_file(dir_itr->status()))
+                {
+                    ++file_count;
+                    raster_files.push_back(dir_itr->path().filename().string());
+                }
+            }
+            catch (const std::exception & ex)
+            {
+                ++err_count;
+                std::cout << dir_itr->path().filename() << " " << ex.what() << std::endl;
+            }
+        }
+        std::cout << "Found " << file_count << " files with " << err_count << " errors:" << std::endl;
+    }
+    else // must be a file
+    {
+        std::cout << "\nFound: " << p << "\n";
+    }
+}
--- include/file_utils.h
+++ include/file_utils.h
@@ -1,8 +1,8 @@
-//
-// Created by jlehtoma on 1/28/16.
-//
-
 #ifndef GEODRILL_FILE_UTILS_H
 #define GEODRILL_FILE_UTILS_H
+
+#include <string>
+
+void list_files(std::string dir, std::vector<std::string>& raster_files);
 
 #endif //GEODRILL_FILE_UTILS_H
