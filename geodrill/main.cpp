a3483527d93931c1c83bead38087dc4e095b1790
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2016-01-28 17:36:44 +0200

    Deal with file listing from the CLI

--- geodrill/main.cpp
+++ geodrill/main.cpp
@@ -6,7 +6,7 @@
 #include <boost/program_options/variables_map.hpp>
 
 #include "GeoDrillConfig.h"
-#include "io.h"
+#include "file_utils.h"
 
 namespace po = boost::program_options;
 
@@ -16,11 +16,12 @@ int main(int argc, const char* argv[]){
     description.add_options()
             ("help,h", "Display this help message")
             /* ("compression,c", po::value<int>()->default_value(5)->implicit_value(10),"Compression level") */
-            ("input-files", po::value<std::vector<std::string>>(), "Input files")
+            ("input-dir", po::value<std::vector<std::string>>(), "Input directory")
+            ("list,l", po::bool_switch()->default_value(false), "List files")
             ("version,v", "Display the version number");
 
     po::positional_options_description p;
-    p.add("input-files", -1);
+    p.add("input-dir", -1);
 
     po::variables_map vm;
     po::store(po::command_line_parser(argc, argv).options(description).positional(p).run(), vm);
@@ -45,12 +46,17 @@ int main(int argc, const char* argv[]){
     }
 
     int success;
-    if(vm.count("input-files")){
-        std::vector<std::string> files = vm["input-files"].as<std::vector<std::string>>();
-        for(std::string file : files){
-            std::cout << "Input file " << file << std::endl;
-            const char * pszFilename = file.c_str();
-            success = printRasterInfo(pszFilename);
+    if(vm.count("input-dir")) {
+        std::vector<std::string> dirs = vm["input-dir"].as<std::vector<std::string>>();
+        std::vector<std::string> input_rasters;
+        for (std::string dir : dirs) {
+            list_files(dir, input_rasters);
+        }
+        for (std::string raster_file : input_rasters) {
+            if (vm.count("list") && vm["list"].as<bool>()) {
+                std::cout << "  " << raster_file << std::endl;
+            }
+
         }
     }
 
