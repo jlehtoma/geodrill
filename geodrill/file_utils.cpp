ed2cc8d2bad86e69230b5215c11998ea618bc897
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2016-01-28 20:45:11 +0200

    Add boost_system to deps

--- geodrill/file_utils.cpp
+++ geodrill/file_utils.cpp
@@ -10,13 +10,7 @@
 
 #define BOOST_FILESYSTEM_VERSION 3
 
-//  As an example program, we don't want to use any deprecated features
-#ifndef BOOST_FILESYSTEM_NO_DEPRECATED
-#  define BOOST_FILESYSTEM_NO_DEPRECATED
-#endif
-#ifndef BOOST_SYSTEM_NO_DEPRECATED
-#  define BOOST_SYSTEM_NO_DEPRECATED
-#endif
+
 
 #include "boost/filesystem/operations.hpp"
 #include "boost/filesystem/path.hpp"
