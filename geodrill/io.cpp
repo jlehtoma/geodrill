13da864e6170f3a0b4892b33677a6e78dee932d9
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2016-01-29 09:05:28 +0200

    Add comments

--- geodrill/io.cpp
+++ geodrill/io.cpp
@@ -43,7 +43,20 @@ int printRasterInfo(const char* pszFilename)
 
 int readRaster(const char* pszFilename)
 {
+    // Set config options for GDAL (needs >= 2.0). Setting GTIFF_VIRTUAL_MEM_IO to "YES" can cause things bleed to
+    // swap if enough RAM is not available. Use "IF_ENOUGH_RAM" for safer performance if unsure. NOTE that faster
+    // mem io only works with *uncompressed* GeoTIFFs.
+    // New in GDAL 2.0, from https://2015.foss4g-na.org/sites/default/files/slides/GDAL%202.0%20overview.pdf
+    // GeoTIFF driver (with i7-4700 HQ (8 vCPUs)):with i7-4700 HQ (8 vCPUs)
+    //   - Default: time ./testblockcache -ondisk: 7.5s
+    //   - GTIFF_DIRECT_IO=YES: short circuit the block cache&libtiff for most RasterIO() operations (restricted to
+    //     uncompressed stripped GeoTIFF). time ./testblockcache -ondisk: 2s
+    //   - GTIFF_VIRTUAL_MEM_IO=YES: same as above, with tiled GeoTIFF as well. Uses memory-mapped file access. Linux
+    //     only for now, 64bit recommended (could be extended to other platforms possible).
+    //     time ./testblockcache -ondisk: 0.3s
+    //
     CPLSetConfigOption("GTIFF_VIRTUAL_MEM_IO", "YES" );
+    // Open the dataset
     GDALDataset *poDataset;
     GDALAllRegister();
     poDataset = (GDALDataset *) GDALOpen( pszFilename, GA_ReadOnly );
@@ -60,6 +73,7 @@ int readRaster(const char* pszFilename)
         int             bGotMin, bGotMax;
         double          adfMinMax[2];
 
+        //  Get raster band and its size
         poBand = poDataset->GetRasterBand(1);
         poBand->GetBlockSize( &nBlockXSize, &nBlockYSize);
 
@@ -69,6 +83,7 @@ int readRaster(const char* pszFilename)
                 GDALGetDataTypeName(poBand->GetRasterDataType()) << " ColorInterp=" <<
                 GDALGetColorInterpretationName(poBand->GetColorInterpretation()) << std::endl;
 
+        // Calculate some stats
         adfMinMax[0] = poBand->GetMinimum(&bGotMin);
         adfMinMax[1] = poBand->GetMaximum(&bGotMax);
 
@@ -85,18 +100,23 @@ int readRaster(const char* pszFilename)
                     " entries." << std::endl;
         }
 
+        // Get the actual data
         float *pafScanline;
         int   nXSize = poBand->GetXSize();
         pafScanline = (float *) CPLMalloc(sizeof(float) * nXSize);
 
-        // RasterIO has a new argument psExtraArg in GDAL > 2.0
+        // RasterIO has a new argument psExtraArg in GDAL > 2.0. NOTE: that  GDALRasterBand::ReadBlock() probably has
+        // better performance for reading the whole data at one go.
         #ifdef USE_GDAL_2
             GDALRasterIOExtraArg* arg = NULL;function call arguments different library version
-            poBand->RasterIO(GF_Read, 0, 0, nXSize, 1, pafScanline, nXSize, 1, GDT_Float32, 0, 0, arg);
+            poBand->RasterIO(GF_Read, 0, 0, nXSize, 1, pafScanline, nXSize, 1, GDT_Float3GDALRasterBand::ReadBlock	2, 0, 0, arg);
         #else
             poBand->RasterIO(GF_Read, 0, 0, nXSize, 1, pafScanline, nXSize, 1, GDT_Float32, 0, 0);
         #endif
 
+        // ... do something with the data ...
+
+        // Free resources
         CPLFree(pafScanline);
         GDALClose((GDALDatasetH) poDataset);
         std::cout << std::endl;
